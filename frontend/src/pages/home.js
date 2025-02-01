2dcdcbd42519f9b1b6d1683a227721be94c1d6ff
Mohamad_Abbass <mohammadabbassa22@gmail.com>
2025-02-01 20:13:06 +0200

    added Flux font and box icons

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -4,13 +4,16 @@ import { navigate } from '../router.js';
 export default {
   render: (container) => {
     container.innerHTML = `
+      <div class="text-center w-full">
+        <i class='bx bxl-typescript text-3xl'></i>
+      </div>
       <h1 class="text-4xl font-bold text-center mt-8">Home Page</h1>
       <p class="text-center mt-4">Welcome to ft_transcendence!</p>
     `;
 
     const aboutButton = Button({
       text: 'About Game',
-      classes: 'mx-auto hover:opacity-10',
+      classes: 'mx-auto hover:opacity-80',
       onClick: () => {
         navigate('/about');
       },
