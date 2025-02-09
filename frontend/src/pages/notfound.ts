4dc31ff23934e3657002ba46affdcaa2870108b9
Mohamad_Abbass <mohammadabbassa22@gmail.com>
2025-02-09 20:55:52 +0200

    add chatting page and  add some changes on the main page

--- frontend/src/pages/notfound.ts
+++ frontend/src/pages/notfound.ts
@@ -6,26 +6,17 @@ export default {
 	render: (container:HTMLElement) => {
 	  container.innerHTML = `
 		<div class="h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
-		  <div class="container mx-auto flex justify-start">
+		  <div class="container mx-auto px-2 flex justify-start">
 		  	<i class='back-btn bx bx-left-arrow-alt text-4xl 2xl:text-[5rem] hover:cursor-pointer hover:opacity-80'></i>
 		  </div>
 		  <div class="text-center page-body">
-			<!-- 404 Icon -->
 			<i class='bx bx-ghost text-9xl 2xl:text-[12rem] mb-6 drop-shadow-[1px_1px_20px_white] animate-pulse '></i>
-			
-			<!-- Title -->
 			<h1 class="text-6xl 2xl:text-[6.5rem] font-bold mb-4">404</h1>
-			
-			<!-- Subtitle -->
 			<h2 class="text-2xl 2xl:text-4xl font-semibold mb-8">${msg('notfound.title')}</h2>
-			
-			<!-- Message -->
 			<p class="text-gray-400 mb-8 max-w-md mx-auto 2xl:text-lg">
 				${msg('notfound.message')}
 			</p>
-			
 			<!-- Back to Home Button -->
-
 		  </div>
 		</div>
 	  `;
@@ -40,6 +31,6 @@ export default {
 	  container.querySelector('.page-body')!.appendChild(HomeBtn)
 	  container.querySelector('.back-btn')!.addEventListener('click', () => {
 		history.back();
-	  })
+	  }, {once: true})
 	}
 };
