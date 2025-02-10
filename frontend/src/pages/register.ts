41c2ea89fcdcb85d86e35ab6ef2c5183d2842e23
Mohamad_Abbass <mohammadabbassa22@gmail.com>
2025-02-10 15:52:20 +0200

    Start with Chat core

--- frontend/src/pages/register.ts
+++ frontend/src/pages/register.ts
@@ -9,7 +9,7 @@ export default {
 		<div class="loaded-div fixed inset-0 overflow-hidden">
 			<canvas id="pongCanvas" class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 sm:translate-x-0 sm:translate-y-0 sm:top-0 sm:left-0 z-0 w-[100vh] h-[100vw] sm:w-[100vw] sm:h-[100vh] inset-0 rotate-90 origin-center sm:rotate-0"></canvas>
 			<div class="relative z-10 flex items-center justify-center">
-				<section class="flex items-center justify-center h-screen opacity-90">
+				<section class="flex items-center justify-center h-[100dvh] opacity-90">
 					<div class="bg-white shadow-[0_0_10px_rgba(0,0,0,0.15)] shadow-white rounded-lg">
 						<aside class="transition-opacity duration-400">
 						</aside>
