01f9eb24ec062141a7a25c4513196c6bc8e227d3
Mohamad_Abbass <mohammadabbassa22@gmail.com>
2025-02-12 19:53:23 +0200

    to be continued <Chat Page>

--- frontend/src/components/Chat.ts
+++ frontend/src/components/Chat.ts
@@ -7,8 +7,8 @@ interface ChatProps {
 export const Chat = createComponent((props: ChatProps) => {
     const container = document.createElement('div')
     container.innerHTML = `
-        <div  class="flex flex-col justify-between px-2 sm:px-4 h-screen">
-            <div class="flex gap-1 h-fit items-center py-3 px-2 bg-white">
+        <div  class="flex flex-col bg-[var(--main-color)] bg-custom-gradient justify-between px-1 sm:px-2 h-screen z-20">
+            <div class="flex gap-1 rounded-full h-fit items-center justify-between py-3 px-2 bg-white">
                 <div class="back_arrow sm:hidden text-black text-3xl flex items-center justify-center hover:cursor-pointer hover:opacity-80">
                     <i class='bx bx-left-arrow-alt'></i>
                 </div>
@@ -18,13 +18,17 @@ export const Chat = createComponent((props: ChatProps) => {
                         <p class="friend_name text-lg sm:text-xl">Test User<!-- Name of the Friend Here --></p>
                     </div>
                 </div>
+                <div class="flex items-center gap-2 justify-center">
+                    <button type="button">Block</button>
+                    <button type="button">Invite</button>
+                </div>
             </div>
-            <section id ="message-container" class="chat_core overflow-y-auto flex-1 flex-row">
+            <section id ="message-container" class="chat_core overflow-y-auto styled-scrollbar h-fit flex-1 flex flex-col-reverse">
             
             </section>
             <div id="message-container" class="flex items-center h-fit bg-[var(--bg-color)] gap-2 w-full rounded-full mb-16 sm:mb-4">
-                <form id="send-container" >
-                    <input type="text" placeholder="Type your message..." id="message-input" class="focus:outline-none rounded-full bg-[var(--bg-color)] p-3 text-lg text-white flex-1">
+                <form id="send-container" class="flex items-center w-full" autocomplete="off" >
+                    <input type="text" placeholder="Type your message..." id="message-input" autocomplete="off" class="focus:outline-none rounded-full bg-[var(--bg-color)] p-3 text-lg text-white flex-1" autocorrect="off" autocapitalize="off" spellcheck="false">
                     <button type="submit" id="send-button" class="flex pr-3 items-center justify-center hover:cursor-pointer hover:opacity-80 rounded-full h-8 w-8 sm:w-12 sm:h-12 text-2xl text-white bg-[var(--bg-color)]">
                         <i class='bx bx-send'></i>
                     </button>
