eaf327deda10f870ab87979130446f508d7efdb4
ahmad farachi <103252037+ahmadfa-lb@users.noreply.github.com>
2025-02-16 12:19:48 +0200

    frontend:creating UserInfo component

--- frontend/src/components/UserProfile.ts
+++ frontend/src/components/UserProfile.ts
@@ -6,6 +6,7 @@ import silverRank from "../../public/assets/silver-medal.png";
 import bronzeRank from "../../public/assets/bronze-medal.png";
 import { navigate, refreshRouter } from "../router.js";
 import { GamesHistory } from "./GmaesHistory";
+import { UserInfo } from "./UserInfo";
 
 export const Profile = createComponent(() => {
   // A wrapper for our popup + overlay
@@ -120,59 +121,8 @@ export const Profile = createComponent(() => {
     if (!contentContainer) return;
     clearActiveTabs();
     infoTab.classList.add("bg-[var(--main-color)]");
-    contentContainer.innerHTML = `
-    <div class="flex flex-col">    
-        <div class="flex space-x-4">
-            <div>
-                <label class="block font-semibold">First name:</label>
-                <input type="text" class="border border-gray-300 p-1 w-48" placeholder="First name">
-            </div>
-            <div>
-                <label class="block font-semibold">Last name:</label>
-                <input type="text" class="border border-gray-300 p-1 w-48" placeholder="Last name">
-            </div>    
-            <div>
-                <label class="block font-semibold">Age:</label>
-                <input type="text" class="border border-gray-300 p-1 w-48" placeholder="Age">
-            </div>
-        </div>
-
-        <div class="flex space-x-4 mt-2">
-            <div>
-                <label class="block font-semibold">Country:</label>
-                <input type="text" class="border border-gray-300 p-1 w-48" placeholder="Country">
-            </div>
-            <div>
-                <label class="block font-semibold">Email:</label>
-                <input type="text" class="border border-gray-300 p-1 w-48" placeholder="Email">
-            </div>
-            <div>
-                <label class="block font-semibold">Gender:</label>
-                <input type="text" class="border border-gray-300 p-1 w-48" placeholder="Male/Female">
-            </div>
-        </div>
-        
-        <!-- 2FA Toggle -->
-        <div class="flex flex-col justify-start gap-2 mt-4">
-            <span class="font-semibold">Enable 2FA</span>
-            <label class="relative inline-flex items-center cursor-pointer">
-                <input type="checkbox" id="twoFactorToggle" class="sr-only peer" />
-                <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 
-                peer-focus:ring-blue-300 rounded-full peer 
-                peer-checked:after:translate-x-full 
-                peer-checked:after:border-white 
-                after:content-[''] after:absolute after:top-0.5 after:left-[2px] 
-                after:bg-white after:border-gray-300 after:border 
-                after:rounded-full after:h-5 after:w-5 after:transition-all
-                peer-checked:bg-blue-600"></div>
-            </label>
-        </div>
-        <!-- Save Button on the right side -->
-        <div class="flex justify-end mt-4">
-            <button type="submit" class="bg-[var(--main-color)] p-1 w-40 text-white">Save</button>
-        </div>
-    </div>
-    `;
+    contentContainer.innerHTML = "";
+    contentContainer?.appendChild(UserInfo());
 
     const toggle = contentContainer.querySelector("#twoFactorToggle") as HTMLInputElement | null;
     if (toggle) {
