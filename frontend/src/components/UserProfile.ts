e76a39dfa075fda4d8b9c9ee0e67e948df10b50f
ahmad farachi <103252037+ahmadfa-lb@users.noreply.github.com>
2025-02-16 12:14:26 +0200

    frontend:creating GmaesHistory component

--- frontend/src/components/UserProfile.ts
+++ frontend/src/components/UserProfile.ts
@@ -5,6 +5,7 @@ import goldRank from "../../public/assets/gold-medal.png";
 import silverRank from "../../public/assets/silver-medal.png";
 import bronzeRank from "../../public/assets/bronze-medal.png";
 import { navigate, refreshRouter } from "../router.js";
+import { GamesHistory } from "./GmaesHistory";
 
 export const Profile = createComponent(() => {
   // A wrapper for our popup + overlay
@@ -111,53 +112,8 @@ export const Profile = createComponent(() => {
     if (!contentContainer) return;
     clearActiveTabs();
     historyTab.classList.add("bg-[var(--main-color)]");
-    contentContainer.innerHTML = `
-    <h2 class="text-lg font-bold mb-2">History</h2>
-    <div class="max-h-64 overflow-y-auto">
-      <table class="min-w-full divide-y divide-gray-200">
-        <thead class="bg-gray-50">
-          <tr>
-            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Game</th>
-            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Result</th>
-            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Outcome</th>
-            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Played</th>
-            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Duration</th>
-          </tr>
-        </thead>
-        <tbody class="bg-white divide-y divide-gray-200">
-          <tr class="bg-green-100">
-            <td class="px-6 py-4 whitespace-nowrap">User1 vs User2</td>
-            <td class="px-6 py-4 whitespace-nowrap">10 - 7</td>
-            <td class="px-6 py-4 whitespace-nowrap">Win</td>
-            <td class="px-6 py-4 whitespace-nowrap">1h ago</td>
-            <td class="px-6 py-4 whitespace-nowrap">6 min</td>
-          </tr>
-          <tr class="bg-red-100">
-            <td class="px-6 py-4 whitespace-nowrap">User3 vs User4</td>
-            <td class="px-6 py-4 whitespace-nowrap">7 - 10</td>
-            <td class="px-6 py-4 whitespace-nowrap">Lose</td>
-            <td class="px-6 py-4 whitespace-nowrap">1w ago</td>
-            <td class="px-6 py-4 whitespace-nowrap">8 min</td>
-          </tr>
-          <tr class="bg-green-100">
-            <td class="px-6 py-4 whitespace-nowrap">User7 vs User8</td>
-            <td class="px-6 py-4 whitespace-nowrap">10 - 8</td>
-            <td class="px-6 py-4 whitespace-nowrap">Win</td>
-            <td class="px-6 py-4 whitespace-nowrap">1mo ago</td>
-            <td class="px-6 py-4 whitespace-nowrap">5 min</td>
-          </tr>
-          <tr class="bg-red-100">
-            <td class="px-6 py-4 whitespace-nowrap">User5 vs User6</td>
-            <td class="px-6 py-4 whitespace-nowrap">6 - 10</td>
-            <td class="px-6 py-4 whitespace-nowrap">Lose</td>
-            <td class="px-6 py-4 whitespace-nowrap">2y ago</td>
-            <td class="px-6 py-4 whitespace-nowrap">7 min</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-
-    `;
+    contentContainer.innerHTML = "";
+    contentContainer?.appendChild(GamesHistory());
   });
 
   infoTab?.addEventListener("click", () => {
