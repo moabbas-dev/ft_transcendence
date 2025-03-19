b44521cc44b1900dd15db413dc6035dec5503150
jihadfatfat99 <jihadfatfatt99@gmail.com>
2025-03-19 22:39:22 +0200

    updating 2 factor in frontend

--- frontend/src/pages/register.ts
+++ frontend/src/pages/register.ts
@@ -1,14 +1,13 @@
 import { SignIn } from '../components/register/SignIn.js';
 import { SignUp } from '../components/register/SignUp.js';
 import { PongAnimation } from '../components/partials/PingPongAnimation.js';
-import { msg } from '../languages/LanguageController.js';
 import { SendEmail } from '../components/register/SendEmail.js';
 import { ResetPass } from '../components/register/ResetPass.js';
 import { TwoFactorSend } from '../components/register/TwoFactor.js';
 import { UserInfoForm } from '../components/register/UserInfoForm.js';
 
 export default {
-	render: (container:HTMLElement, params?: { [key: string]: string | number }) => {
+	render: (container: HTMLElement, params?: { [key: string]: string | number }) => {
 		container.innerHTML = `
 		<div class="loaded-div fixed inset-0 overflow-hidden">
 			<canvas id="pongCanvas" class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 sm:translate-x-0 sm:translate-y-0 sm:top-0 sm:left-0 z-0 w-[100vh] h-[100vw] sm:w-[100vw] sm:h-[100vh] inset-0 rotate-90 origin-center sm:rotate-0"></canvas>
@@ -24,37 +23,37 @@ export default {
 		</div>
 		`;
 
-		const animateTransition = (newComponentFn:any) => {
+		const animateTransition = (newComponentFn: any) => {
 			section.classList.add('opacity-0');
 			setTimeout(() => {
-			  section.innerHTML = '';
-			  section.appendChild(newComponentFn());
-			  section.classList.remove('opacity-0');
+				section.innerHTML = '';
+				section.appendChild(newComponentFn());
+				section.classList.remove('opacity-0');
 			}, 400);
 		};
 
-		const section:HTMLElement = container.querySelector('aside')!;
+		const section: HTMLElement = container.querySelector('aside')!;
 		const renderSignIn = () => {
 			animateTransition(() =>
-			  SignIn({
-				styles: 'mx-auto',
-				onSwitchToSignUp: renderSignUp, 
-				onSwitchToResetPass: renderSendEmaiForReset,
-			  })
+				SignIn({
+					styles: 'mx-auto',
+					onSwitchToSignUp: renderSignUp,
+					onSwitchToResetPass: renderSendEmaiForReset,
+				})
 			);
-		  };
-	  
-		  const renderSignUp = () => {
+		};
+
+		const renderSignUp = () => {
 			animateTransition(() =>
-			  SignUp({
-				styles: 'mx-auto',
-				onSwitchToSignIn: renderSignIn,
-			  })
+				SignUp({
+					styles: 'mx-auto',
+					onSwitchToSignIn: renderSignIn,
+				})
 			);
-		  };
+		};
 
 		const renderSendEmaiForReset = () => {
-			animateTransition(() => 
+			animateTransition(() =>
 				SendEmail({
 					onSwitchToSignIn: renderSignIn,
 				})
@@ -62,31 +61,31 @@ export default {
 		}
 
 		const renderResetPass = (params?: { [key: string]: string | number }) => {
-			animateTransition(() => 
+			animateTransition(() =>
 				ResetPass(params)
 			)
 		}
 
 		const renderUserInfo = () => {
-			animateTransition(() => 
+			animateTransition(() =>
 				UserInfoForm()
 			)
 		}
 
 		const renderTwoFactor = () => {
-			animateTransition(() => 
+			animateTransition(() =>
 				TwoFactorSend(
-					{onSwitchToSignIn: renderSignIn}
+					{ onSwitchToSignIn: renderSignIn }
 				)
 			)
 		}
-		// renderTwoFactor()
-		if (!params || Object.keys(params).length === 0)
-			renderSignIn();
-		else
+
+		if (params?.uuid)
 			renderResetPass(params);
-		// renderUserInfo();
-		// renderSignUp()
+		else if (window.location.pathname === "/register/twofactor")
+			renderTwoFactor();
+		else
+			renderSignIn();
 
 		const canvas = document.getElementById('pongCanvas') as HTMLCanvasElement;
 		if (canvas)
