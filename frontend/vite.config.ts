999969bd053f1c0a17086bea2753b03468fe6be0
moabbas-dev <mohammadabbassa33@gmail.com>
2025-03-27 15:02:35 +0200

    fix countries

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  plugins: [mkcert()],
-  server: {
-    https: {},
-  },
+  plugins: [],
+  // plugins: [mkcert()],
+  // server: {
+  //   https: {},
+  // },
 })
